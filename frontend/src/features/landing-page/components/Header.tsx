690bd844cbf7bb086472b0a9e38f64b06a4a457d
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-10-23 17:34:45 +0100

    feat(env): add environment configuration and database settings to .env.example
    feat(docker): create docker-compose file for API and database services with health checks
    
    chore(gitignore): add .gitignore for backend and frontend files
    
    fix(assets): update logo images in Header component and adjust layout

--- frontend/src/features/landing-page/components/Header.tsx
+++ frontend/src/features/landing-page/components/Header.tsx
@@ -2,14 +2,18 @@ import { ROUTE_PATHS } from "@/config/routes";
 import { Link } from "@tanstack/react-router";
 import { LanguageSwitcher } from "@/components/ui";
 import { useTranslation } from "react-i18next";
-import FullLogoHorizontal from "@/assets/FullLogoHorizontal.png";
+import Icon1 from "@/assets/Icon-1.png";
+import TextIcon from "@/assets/TextIcon .png";
 
 export function Header() {
   const { t } = useTranslation();
 
   return (
     <div className="flex items-center justify-between px-1 py-8">
-      <img src={FullLogoHorizontal} alt="FullLogoHorizontal" />
+      <div className="flex items-center">
+        <img src={Icon1} alt="" className="h-20" />
+        <img src={TextIcon} alt="" className="h-20" />
+      </div>
 
       <div className="flex items-center gap-4">
         <LanguageSwitcher />
