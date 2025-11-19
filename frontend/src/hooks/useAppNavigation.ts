284348bc16617c1b697da54d880cdd819c04e94b
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-11-19 11:19:10 +0100

    feat(navigation): update goToPatientApp to navigate to patient root route

--- frontend/src/hooks/useAppNavigation.ts
+++ frontend/src/hooks/useAppNavigation.ts
@@ -48,7 +48,7 @@ export function useAppNavigation() {
     },
 
     goToPatientApp: () => {
-      navigate({ to: ROUTE_PATHS.ROOT }); 
+      navigate({ to: ROUTE_PATHS.PATIENT.ROOT });
     },
 
     goBack: () => {
