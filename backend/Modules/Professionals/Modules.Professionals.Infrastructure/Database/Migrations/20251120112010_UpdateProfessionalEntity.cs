6aa3a74405c50a5e24b1746b70b5e779f2c8e320
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-11-21 08:27:16 +0100

    feat(professional): update Professional entity with new fields and rename years_of_experience to experience

---
+++ backend/Modules/Professionals/Modules.Professionals.Infrastructure/Database/Migrations/20251120112010_UpdateProfessionalEntity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Professionals.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProfessionalEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "years_of_experience",
+                schema: "professionals",
+                table: "professionals",
+                newName: "start_price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "bio",
+                schema: "professionals",
+                table: "professionals",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "end_price",
+                schema: "professionals",
+                table: "professionals",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "experience",
+                schema: "professionals",
+                table: "professionals",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "services",
+                schema: "professionals",
+                table: "professionals",
+                type: "text[]",
+                nullable: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                schema: "professionals",
+                table: "professionals",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bio",
+                schema: "professionals",
+                table: "professionals");
+
+            migrationBuilder.DropColumn(
+                name: "end_price",
+                schema: "professionals",
+                table: "professionals");
+
+            migrationBuilder.DropColumn(
+                name: "experience",
+                schema: "professionals",
+                table: "professionals");
+
+            migrationBuilder.DropColumn(
+                name: "services",
+                schema: "professionals",
+                table: "professionals");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                schema: "professionals",
+                table: "professionals");
+
+            migrationBuilder.RenameColumn(
+                name: "start_price",
+                schema: "professionals",
+                table: "professionals",
+                newName: "years_of_experience");
+        }
+    }
+}
