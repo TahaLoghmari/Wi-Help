b5c1097c386320a48f71dab5540d43196abba9c9
Mohamed Yassine landolsi <yesslandolsi@gmail.com>
2025-11-23 18:20:20 +0100

    comment card stats

--- frontend/src/features/professional/appointments/components/AppointmentsLayout.tsx
+++ frontend/src/features/professional/appointments/components/AppointmentsLayout.tsx
@@ -8,11 +8,11 @@ import {
 export function AppointmentsLayout() {
   return (
     <div className="flex h-full w-full flex-col gap-5 bg-[#fafafb] px-8 py-5">
-      <div className="grid grid-cols-3 gap-4">
+      {/* <div className="grid grid-cols-3 gap-4">
         <TotalPatientsStat />
         <TodayPatientsStat />
         <AppointmentsStat />
-      </div>
+      </div> */}
       <AppointmentsTable />
     </div>
   );
