6af9478314cf9767a16eec5ef301f2ce097410a4
Mohamed Yassine landolsi <yesslandolsi@gmail.com>
2025-11-23 23:35:44 +0100

    migrate

---
+++ backend/Modules/Patients/Modules.Patients.Infrastructure/Database/Migrations/20251123223536_YeahUpdate.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Patients.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class YeahUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_medications",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_chronic_conditions",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_allergies",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_medications",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_chronic_conditions",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "medical_info_allergies",
+                schema: "patients",
+                table: "patients",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+        }
+    }
+}
