4cfa964c4c421d9dee9d8d4abafe0d5a5899d23b
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-11-25 16:17:38 +0100

    feat: Remove RespondToAppointmentAsync method from AppointmentsModuleApi and IAppointmentsModuleApi

--- backend/Modules/Appointments/Modules.Appointments.PublicApi/IAppointmentsModuleApi.cs
+++ backend/Modules/Appointments/Modules.Appointments.PublicApi/IAppointmentsModuleApi.cs
@@ -4,9 +4,4 @@ namespace Modules.Appointments.PublicApi;
 
 public interface IAppointmentsModuleApi
 {
-    public Task<Result> RespondToAppointmentAsync(
-        Guid appointmentId,
-        Guid professionalId,
-        bool isAccepted,
-        CancellationToken cancellationToken = default);
 }
